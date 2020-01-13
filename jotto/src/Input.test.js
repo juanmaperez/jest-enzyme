b5c18924c684768a8824f7e3210ea4036930e1b3
juanmaperez <juanmaperezvar@gmail.com>
2020-01-13 19:39:35 +0000

    rename files and use dive functions

---
+++ jotto/src/Input.test.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+
+import { findElement, storeFactory }from './../test/testUtils'
+import Input from './Input'
+
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState)
+  const wrapper = shallow(<Input store={store} />).dive().dive()
+  return wrapper;
+}
+
+setup()
+
+describe('Input', () => {
+  describe('renders', () => {
+    describe('word has not been guessed', ()=> {
+  
+      test('renders component without errors', () => {})
+  
+      test('renders input box', () => {})
+  
+      test('renders submit button', () => {})
+    
+    })
+  
+    describe('word has not been guessed', () => {
+      test('renders component without errors', () => {})
+  
+      test('does not renders input box', () => {})
+  
+      test('does not renders submit button', () => {})
+    })
+  })
+  
+  describe('update state', () => {
+  
+  })
+})
