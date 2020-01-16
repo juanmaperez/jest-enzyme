e9219eb498bae70491000659c26127bc45f0a08c
juanmaperez <juanmaperezvar@gmail.com>
2020-01-16 21:03:07 +0000

    connect App component and create tests

--- jotto/src/App.js
+++ jotto/src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { getSecretWord } from './actions'
 import './App.css';
 import GuessedWords from './GuessedWords'
 import Congrats from './Congrats'
 import Input from './Input'
 
 class App extends Component {
+
+  componentDidMount() {
+    const { getSecretWord } = this.props
+    getSecretWord()
+  }
+
   render(){
+    const { success, guessedWords, secretWord } = this.props
     return (
       <div className="app container">
         <h1 className="row">Jotto</h1>
         <Input/>
-        <Congrats success={false}/>
-        <GuessedWords guessedWords={[]}/>
+        <Congrats success={success}/>
+        <GuessedWords guessedWords={guessedWords}/>
       </div>
     );
   }
 
 }
 
-export default App;
+const mapStateToProps = ({ guessedWords, secretWord, success}) => {
+  return {
+    guessedWords,
+    secretWord,
+    success
+  }
+} 
+
+const mapDispatchToProps = dispatch => {
+  return {
+    getSecretWord: () => dispatch(getSecretWord())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- jotto/src/App.test.js
+++ jotto/src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
 import App from './App';
-import { findElement } from './../test/testUtils'
+import { findElement, storeFactory } from './../test/testUtils'
 /** 
 * Setup function to create a shallowWrapper of App Component
 * @function setup
@@ -10,14 +10,50 @@ import { findElement } from './../test/testUtils'
 * @returns { ShallowWrapper}
 */
 
-const setup = (props = {}, state = null) => {
-  const wrapper = shallow(<App {...props} />)
-  if(state) wrapper.setState(state)
+const setup = ( state = {} ) => {
+  const store = storeFactory(state)
+  const wrapper = shallow(<App store={store} />).dive().dive()
   return wrapper
 }
 
+describe('<App />', () => {
+  let wrapper
+  let secretWord
+  let guessedWords
+  let success
+  beforeEach(() => {
+    secretWord = 'party';
+    guessedWords = []
+    success = true
+    wrapper = setup({
+      secretWord,
+      guessedWords,
+      success
+    })
+  })
+  test('renders without an error', () => {
+    expect(findElement(wrapper, '.app').length).toBe(1)
+  });
 
-test('renders without an error', () => {
-  const wrapper = setup()
-  expect(findElement(wrapper, '.app').length).toBe(1)
-});
+  test('receives the `success` as a prop', () => {
+    const successProp = wrapper.instance().props.success
+    expect(successProp).toBe(success)
+  })
+
+  test('receives the `guessedWords` as a prop', () => {
+    const guessedWordsProp = wrapper.instance().props.guessedWords
+    expect(guessedWordsProp).toEqual(guessedWords)
+  })
+
+  test('receives the `secretWord` as a prop', () => {
+    const secretWordProp = wrapper.instance().props.secretWord
+    expect(secretWordProp).toBe(secretWord)
+  })
+
+  test('it receives getSecretWord prop as function', () => {
+    const getSecretWordProp = wrapper.instance().props.getSecretWord
+    expect(getSecretWordProp).toBeInstanceOf(Function)
+  })
+})
+
+
