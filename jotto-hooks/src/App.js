254c33fac8a2e5feff0c73a20d3a01873ff6a463
juanmaperez <juanmaperezvar@gmail.com>
2020-01-29 14:37:27 +0000

    getSecretWord runs on App mount code and test

--- jotto-hooks/src/App.js
+++ jotto-hooks/src/App.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import './App.css';
 import Input from './Input'
+import hookActions from './actions/hookActions';
+
+const initialState = { secretWord: null }
+
+/**
+ * Reducer to update the state called automatically by dispatch
+ * @param { object } state existing state 
+ * @param { object } action object cointaining the type and the payload 
+ *                   for example { type: 'SET_SECRET_WORD, payload: 'party'}
+ * @returns {object} new state
+ */
+const reducer = (state, action) => {
+  switch(action.type){
+    case 'SET_SECRET_WORD':
+      return {
+        ...state, 
+        secretWord: action.payload
+      }
+    default: 
+      return state
+  }
+}
 
 function App() {
+
+  const [state, dispatch] = React.useReducer(
+    reducer,
+    initialState
+  )
+
+  const setSecretWord = (secretWord) => dispatch({ type: 'SET_SECRET_WORD', payload: secretWord})
+
+  React.useEffect(() => {
+    hookActions.getSecretWord(setSecretWord)
+  }, [])
+
   return (
     <div className="App">
       <Input secretWord="party"/>
--- jotto-hooks/src/App.test.js
+++ jotto-hooks/src/App.test.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
 import { findElement } from './../test/testUtils'
 import App from './App';
+import hookActions from './actions/hookActions'
 
-const setup = () => shallow(<App/>)
+const mockGetSecretWord = jest.fn()
+
+/**
+ * Setup function for App Component
+ * @returns { ReactWrapper }
+ */
+const setup = () => {
+  mockGetSecretWord.mockClear()
+  hookActions.getSecretWord =  mockGetSecretWord
+  // use mount because useEffect is not called on shallow
+  // https://github.com/airbnb/enzyme/issues/2086
+  return mount(<App/>)
+}
 
 describe('<App/>', () => {
   test('it renders without errors', () => {
@@ -11,5 +24,13 @@ describe('<App/>', () => {
     const component = findElement(wrapper, '.App')
     expect(component.length).toBe(1)
   });
+
+  describe('getSecretWord calls', () => {
+    test('gets call on App mount', () => {
+      setup()
+      expect(mockGetSecretWord).toHaveBeenCalled()
+    })
+
+  })
 })
 
