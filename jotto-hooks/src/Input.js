270cf9708ec452e552281cf5e0e954bab790c5f4
juanmaperez <juanmaperezvar@gmail.com>
2020-01-29 12:24:03 +0000

    add input current guess updating and test

--- jotto-hooks/src/Input.js
+++ jotto-hooks/src/Input.js
@@ -2,8 +2,29 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Input = ({ secretWord}) => {
+
+  const [currentGuess, setCurrentGuess ] = React.useState('')
+
+  const handleSubmit = () => {
+
+  }
+
   return (
-    <div className="input-component"></div>
+    <div className="input-component">
+      <form className="form-inline">
+        <input
+          className="input mb-2 mx-sm-3" 
+          type="text" 
+          placeholder="Enter guess word" 
+          value={ currentGuess }
+          onChange={ (e) => setCurrentGuess(e.target.value)} 
+        />
+        <button 
+          className="submit btn btn-primary mb-2"
+          onClick={ handleSubmit }
+        >Guess</button>
+      </form>
+    </div>
   )
 }
 
--- jotto-hooks/src/Input.test.js
+++ jotto-hooks/src/Input.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme'
 import { findElement, checkProps } from '../test/testUtils'
 import Input from './Input'
 
-const setup = (props = {}) => {
+const setup = (props = { secretWord: 'party'}) => {
   const wrapper = shallow(<Input {...props } />)
   return wrapper
 }
@@ -19,5 +19,21 @@ describe('<Input/>', () => {
     const wrapper =  setup({ secretWord})
     const component = findElement(wrapper, '.input-component')
     expect(component.length).toBe(1)
+  })
+})
+
+describe('state controlled input field', () => {
+  test('state updates with value of input box upon change', () => {
+    const mockSetCurrentGuess = jest.fn()
+    React.useState = jest.fn(() => ['', mockSetCurrentGuess])
+
+    const wrapper = setup()
+    const inputBox = findElement(wrapper, '.input')
+
+    const mockEvent =  { target: { value: 'train'}}
+
+    inputBox.simulate('change', mockEvent)
+
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith('train')
   })
 })
